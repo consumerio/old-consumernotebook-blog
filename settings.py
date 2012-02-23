be7d007d55fb295ff1d8bf63b2e1fc128b868f11
Audrey Roy <audreyr@gmail.com>
2012-02-22 20:01:52 -0800

    Logo path fix.

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 AUTHOR = 'Consumer Notebook'
 SITEURL = 'http://blog.consumernotebook.com'
 SITENAME = 'Insider Tips For Consumers'
-SITE_LOGO = 'theme/images/logo-142x32.png'
+SITE_LOGO = 'http://blog.consumernotebook.com/theme/images/logo-142x32.png'
 SOCIAL = (('twitter', 'http://twitter.com/consumernotebk'),)
 TAG_FEED = 'feeds/%s.atom.xml'
 THEME='themes/consumernotebook/'
